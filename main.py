98b57a3ca0d65ef63dd7a441f9788635a6486ca8
Anton Patrushev <apatrushev@gmail.com>
2013-05-23 20:04:05 +0600

    working solution

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import os
+import collections
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
+
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
 class RouletteHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        print "WebSocket opened"
+    offers = collections.OrderedDict()
+    peer = dict()
+
     def on_message(self, message):
-        print message
+        print "Message received: " + message
+        if self in RouletteHandler.peer:
+            RouletteHandler.peer[self].write_message(message)
+        elif self in RouletteHandler.offers:
+            RouletteHandler.offers[self].append(message)
+        elif len(RouletteHandler.offers):
+            print "Pair created"
+            sender, offer = RouletteHandler.offers.popitem(False)
+            RouletteHandler.peer[sender] = self
+            RouletteHandler.peer[self] = sender
+            map(self.write_message, offer)
+        else:
+            print "New offer registered"
+            RouletteHandler.offers[self] = [message,]
+
     def on_close(self):
-        print "WebSocket closed"
+        if self in RouletteHandler.offers:
+            del RouletteHandler.offers[self]
+        elif self in RouletteHandler.peer:
+            RouletteHandler.peer[self].close()
+            del RouletteHandler.peer[self]
+
+    def close(self):
+        try:
+            tornado.websocket.WebSocketHandler.close(self)
+        except:
+            pass
 
 root = os.path.join(os.path.dirname(__file__), "static")
 handlers = [
--- static/js/roulette.js
+++ static/js/roulette.js
@@ -1,8 +1,17 @@
 var rouletteWS = null;
 var pc = null;
+var offer = null;
+var localstream = null;
+var sdpConstraints = {
+	'mandatory': {
+		'OfferToReceiveAudio':true,
+		'OfferToReceiveVideo':true
+}};
+var pc_config = {"iceServers": [{"url": "stun:23.21.150.121"}]};
+var pc_constraints = {"optional": [{"DtlsSrtpKeyAgreement": true}]};
 
 function message(text) {
-	$("#message").text(text);
+	$("#message").html(text);
 }
 
 function roulette() {
@@ -10,7 +19,8 @@ function roulette() {
 		message("getUserMedia not supported by your browser");
 		return;
 	}
-    getUserMedia({video:true, audio:false}, localStreamConnected,
+	var constraints = {"audio": false, "video": true};
+    getUserMedia(constraints, localStreamConnected,
         function() {
           message("error accessing video capture device");
         }
@@ -35,7 +45,8 @@ function wsURI(rel) {
 
 function localStreamConnected(stream) {
 	message("Local stream connected");
-	attachMediaStream($("#local_video")[0], stream);
+	localstream = stream;
+	audioTracks = localstream.getAudioTracks();
 	if (!window.WebSocket) {
 		message("WebSocket's not supported by your browser");
 		return;
@@ -47,28 +58,101 @@ function localStreamConnected(stream) {
 
 function webSocketConnected() {
 	message("WebSocket connected");
+	rouletteWS.onopen = null;
 	rouletteWS.onclose = webSocketClosed;
-	var pc_config = {"iceServers": [{"url": "stun:173.194.69.127:19302"}]};
+	rouletteWS.onmessage = webSocketMessage;
 	try {
-		pc = new RTCPeerConnection(pc_config);
-		pc.createOffer(onOffer);
+		pc = new RTCPeerConnection(pc_config, pc_constraints);
+		pc.onicecandidate = onIceCandidate;
+		pc.onaddstream = onRemoteStreamAdded;
+		//pc.onremovestream = onRemoteStreamRemoved;
+
+		pc.addStream(localstream);
+		var constraints = {"optional": [], "mandatory": {"MozDontOfferDataChannel": true}};
+		// temporary measure to remove Moz* constraints in Chrome
+		if (webrtcDetectedBrowser === "chrome") {
+			for (var prop in constraints.mandatory) {
+				if (prop.indexOf("Moz") != -1) {
+					delete constraints.mandatory[prop];
+				}
+			}
+		}
+		constraints = mergeConstraints(constraints, sdpConstraints);
+		pc.createOffer(onOfferCreated, null, constraints);
 	} catch (e) {
 		msg = "Failed to create PeerConnection";
 		if (webrtcDetectedBrowser == "firefox") {
-			msg += ": go to about:config and set media.peerconnection.enabled to true";
+			msg += ': go to about:config and set media.peerconnection.enabled to true';
 		}
 		message(msg);
 	}
 }
 
-function onOffer(sd) {
+function mergeConstraints(cons1, cons2) {
+  var merged = cons1;
+  for (var name in cons2.mandatory) {
+    merged.mandatory[name] = cons2.mandatory[name];
+  }
+  merged.optional.concat(cons2.optional);
+  return merged;
+}
+
+function onOfferCreated(sd) {
+	if (!pc) return;
 	message("Offer created");
+	offer = sd;
+	rouletteWS.send(JSON.stringify(sd));
+}
+
+function onIceCandidate(evt) {
+	if (event.candidate) {
+		rouletteWS.send(JSON.stringify({
+			type: 'candidate',
+			label: event.candidate.sdpMLineIndex,
+			id: event.candidate.sdpMid,
+			candidate: event.candidate.candidate
+		}));
+	}
+}
+
+function onRemoteStreamAdded(event) {
+	message("Remote stream added");
+	attachMediaStream($("#video")[0], event.stream);
+}
+
+function webSocketMessage(evt) {
+	var msg = JSON.parse(evt.data);
+	if (msg.type == "offer") {
+		message("Offer received");
+		pc = new RTCPeerConnection(pc_config, pc_constraints);
+		pc.onicecandidate = onIceCandidate;
+		pc.onaddstream = onRemoteStreamAdded;
+		pc.addStream(localstream);
+		pc.setRemoteDescription(new RTCSessionDescription(msg));
+		pc.createAnswer(onAnswerCreated, null, sdpConstraints);
+	} else if (msg.type == "answer") {
+		message("Answer received");
+		pc.setLocalDescription(offer);
+		pc.setRemoteDescription(new RTCSessionDescription(msg));
+	} else if (msg.type === 'candidate') {
+		var can = new RTCIceCandidate({sdpMLineIndex:msg.label, candidate:msg.candidate});
+		pc.addIceCandidate(can);
+	}
+}
+
+function onAnswerCreated(sd) {
+	if (!pc) return;
+	message("Answer created");
 	pc.setLocalDescription(sd);
 	rouletteWS.send(JSON.stringify(sd));
 }
 
 function webSocketClosed() {
 	message("WebSocket closed");
+	if (pc) {
+		pc.close();
+		pc = null;
+	}
 }
 
 function webSocketError() {
