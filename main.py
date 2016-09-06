e20fa1240bef1235ec40061077a04072c75b90c3
Anton Patrushev <apatrushev@gmail.com>
2016-09-07 01:27:55 +0200

    pretty js code. requirements. click cmdline.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import collections
+import click
 import tornado.ioloop
 import tornado.web
 import tornado.websocket
@@ -9,25 +10,26 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+
 class RouletteHandler(tornado.websocket.WebSocketHandler):
     offers = collections.OrderedDict()
     peer = dict()
 
     def on_message(self, message):
-        print "Message received: " + message
+        print("Message received: " + message)
         if self in RouletteHandler.peer:
             RouletteHandler.peer[self].write_message(message)
         elif self in RouletteHandler.offers:
             RouletteHandler.offers[self].append(message)
         elif len(RouletteHandler.offers):
-            print "Pair created"
+            print("Pair created")
             sender, offer = RouletteHandler.offers.popitem(False)
             RouletteHandler.peer[sender] = self
             RouletteHandler.peer[self] = sender
             map(self.write_message, offer)
         else:
-            print "New offer registered"
-            RouletteHandler.offers[self] = [message,]
+            print("New offer registered")
+            RouletteHandler.offers[self] = [message]
 
     def on_close(self):
         if self in RouletteHandler.offers:
@@ -42,18 +44,26 @@ class RouletteHandler(tornado.websocket.WebSocketHandler):
         except:
             pass
 
-root = os.path.join(os.path.dirname(__file__), "static")
-handlers = [
-    (r"/", MainHandler),
-    (r"^/roulette$", RouletteHandler),
-]
 
-application = tornado.web.Application(
-    handlers,
-    template_path=root,
-    static_path=root
-)
+@click.command()
+@click.option('--port', default=8000, help='Listen port.')
+def main(port):
+    root = os.path.join(os.path.dirname(__file__), "static")
+    handlers = [
+        (r"/", MainHandler),
+        (r"^/roulette$", RouletteHandler),
+    ]
+
+    application = tornado.web.Application(
+        handlers,
+        template_path=root,
+        static_path=root
+    )
+
+    print('Serving HTTP on 0.0.0.0 port {} ...'.format(port))
+    application.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
-    application.listen(8000)
-    tornado.ioloop.IOLoop.instance().start()
+    main()
--- static/js/roulette.js
+++ static/js/roulette.js
@@ -1,35 +1,6 @@
-var rouletteWS = null;
-var pc = null;
-var offer = null;
-var localstream = null;
-var sdpConstraints = {
-	'mandatory': {
-		'OfferToReceiveAudio':false,
-		'OfferToReceiveVideo':true
-}};
-var pc_config = {"iceServers": [{"url": "stun:23.21.150.121"}]};
-var pc_constraints = {"optional": [{"DtlsSrtpKeyAgreement": true}]};
-
-function message(text) {
-	document.getElementById("message").innerHTML = text;
-}
-
-function roulette() {
-	if (!getUserMedia) {
-		message("getUserMedia not supported by your browser");
-		return;
-	}
-	var constraints = { "audio": false, "video": true };
-    getUserMedia(constraints, localStreamConnected,
-        function() {
-          message("error accessing video capture device");
-        }
-    );
-}
-
 function wsURI(rel) {
-	var loc = window.location, new_uri;
-	new_uri = loc.protocol.replace(/^http/, "ws") + "//";
+	var loc = window.location;
+	var new_uri = loc.protocol.replace(/^http/, "ws") + "//";
 	new_uri += loc.host;
 	new_uri += loc.pathname;
 	if (loc.pathname[loc.pathname.length - 1] != "/") {
@@ -39,111 +10,140 @@ function wsURI(rel) {
 	return new_uri;
 }
 
-function localStreamConnected(stream) {
-	message("Local stream connected");
-	localstream = stream;
-	if (!window.WebSocket) {
-		message("WebSocket's not supported by your browser");
-		return;
-	}
-	attachMediaStream(document.getElementById("localVideo"), stream);
-	openSocket();
-}
+roulette = function($, undefined) {
+    var self = this;
+    var sdpConstraints = {
+        'mandatory': {
+            'OfferToReceiveAudio':true,
+            'OfferToReceiveVideo':true
+    }};
+    var pc_config = { "iceServers": [{ "url": "stun:23.21.150.121" }]};
+    var pc_constraints = { "optional": [{ "DtlsSrtpKeyAgreement": true }]};
 
-function openSocket() {
-    rouletteWS = new WebSocket(wsURI("roulette"));
-    rouletteWS.onopen = webSocketConnected;
-    rouletteWS.onclose = webSocketError;
-}
+    self.local_stream = undefined;
+    self.roulette_ws = undefined;
+    self.peer_connection = undefined;
+    self.offer = undefined;
 
-function webSocketConnected() {
-	message("WebSocket connected");
-	rouletteWS.onopen = null;
-	rouletteWS.onclose = webSocketClosed;
-	rouletteWS.onmessage = webSocketMessage;
-	try {
-		pc = new RTCPeerConnection(pc_config, pc_constraints);
-		pc.onicecandidate = onIceCandidate;
-		pc.onaddstream = onRemoteStreamAdded;
-		//pc.onremovestream = onRemoteStreamRemoved;
+    self.add_message = function(text) {
+        $("#message").html(text);
+    }
 
-		pc.addStream(localstream);
-		var constraints = JSON.parse(JSON.stringify(sdpConstraints));
-		// temporary measure to remove Moz* constraints in Chrome
-		if (webrtcDetectedBrowser === "firefox") {
-			constraints.mandatory["MozDontOfferDataChannel"] = true;
-		}
-		pc.createOffer(onOfferCreated, null, constraints);
-	} catch (e) {
-		msg = "Failed to create PeerConnection";
-		if (webrtcDetectedBrowser == "firefox") {
-			msg += ': go to about:config and set media.peerconnection.enabled to true';
-		}
-		message(msg);
-	}
-}
+    self.open_socket = function() {
+        self.roulette_ws = new WebSocket(wsURI("roulette"));
+        self.roulette_ws.onopen = self.on_websocket_open;
+        self.roulette_ws.onclose = self.on_websocket_error;
+    }
 
-function onOfferCreated(sd) {
-	if (!pc) return;
-	message("Offer created");
-	offer = sd;
-	rouletteWS.send(JSON.stringify(sd));
-}
+    self.build_peer_connection = function() {
+        var pc = new RTCPeerConnection(pc_config, pc_constraints);
+        pc.onicecandidate = self.on_ice_candidate;
+        pc.onaddstream = self.on_remotestream_added;
+        pc.addStream(self.local_stream);
+        return pc;
+    }
 
-function onIceCandidate(evt) {
-	if (event.candidate) {
-		rouletteWS.send(JSON.stringify({
-			type: 'candidate',
-			label: event.candidate.sdpMLineIndex,
-			id: event.candidate.sdpMid,
-			candidate: event.candidate.candidate
-		}));
-	}
-}
+    self.on_document_ready = function() {
+        if (!getUserMedia) {
+            message("getUserMedia not supported by your browser");
+            return;
+        }
 
-function onRemoteStreamAdded(event) {
-	message("Remote stream added");
-	attachMediaStream(document.getElementById("remoteVideo"), event.stream);
-}
+        getUserMedia(
+            { "audio": true, "video": false },
+            self.on_localstream_connected,
+            self.on_localstream_error
+       );
+    }
 
-function webSocketMessage(evt) {
-	var msg = JSON.parse(evt.data);
-	if (msg.type == "offer") {
-		message("Offer received");
-		pc = new RTCPeerConnection(pc_config, pc_constraints);
-		pc.onicecandidate = onIceCandidate;
-		pc.onaddstream = onRemoteStreamAdded;
-		pc.addStream(localstream);
-		pc.setRemoteDescription(new RTCSessionDescription(msg));
-		pc.createAnswer(onAnswerCreated, null, sdpConstraints);
-	} else if (msg.type == "answer") {
-		message("Answer received");
-		pc.setLocalDescription(offer);
-		pc.setRemoteDescription(new RTCSessionDescription(msg));
-	} else if (msg.type === 'candidate') {
-		var can = new RTCIceCandidate({sdpMLineIndex:msg.label, candidate:msg.candidate});
-		pc.addIceCandidate(can);
-	}
-}
+    self.on_localstream_connected = function(stream) {
+        self.add_message("local stream connected");
+        self.local_stream = stream;
+        if (!window.WebSocket) {
+            self.add_message("WebSocket's not supported by your browser");
+            return;
+        }
+        attachMediaStream($("#localVideo")[0], stream);
+        self.open_socket();
+    }
 
-function onAnswerCreated(sd) {
-	if (!pc) return;
-	message("Answer created");
-	pc.setLocalDescription(sd);
-	rouletteWS.send(JSON.stringify(sd));
-}
+    self.on_localstream_error = function() {
+        self.add_message("error accessing video capture device");
+    }
 
-function webSocketClosed() {
-	message("WebSocket closed");
-	if (pc) {
-		pc.close();
-		pc = null;
-		rouletteWS = null;
-	}
-	openSocket();
-}
+    self.on_websocket_open = function() {
+        self.add_message("WebSocket connected");
+        self.roulette_ws.onopen = null;
+        self.roulette_ws.onclose = self.on_websocket_closed;
+        self.roulette_ws.onmessage = self.on_websocket_message;
+        try {
+            self.peer_connection = self.build_peer_connection();
+            self.peer_connection.createOffer(self.on_offer_created, self.on_offer_failed, sdpConstraints);
+        } catch (e) {
+            self.add_message("Failed to create PeerConnection" + e);
+        }
+    }
 
-function webSocketError() {
-	message("WebSocket connection failed");
-	setTimeout(openSocket, 1000);
-}
+    self.on_websocket_message = function(evt) {
+        var msg = JSON.parse(evt.data);
+        if (msg.type == "offer") {
+            self.add_message("Offer received");
+            self.peer_connection = self.build_peer_connection();
+            self.peer_connection.setRemoteDescription(new RTCSessionDescription(msg));
+            self.peer_connection.createAnswer(self.on_answer_created, self.on_answer_failed, sdpConstraints);
+            self.offer = undefined;
+        } else if (msg.type == "answer") {
+            self.add_message("Answer received");
+            self.peer_connection.setLocalDescription(offer);
+            self.peer_connection.setRemoteDescription(new RTCSessionDescription(msg));
+        } else if (msg.type === 'candidate') {
+            var candidate = new RTCIceCandidate({sdpMLineIndex: msg.label, candidate: msg.candidate});
+            self.peer_connection.addIceCandidate(candidate);
+        }
+    }
+
+    self.on_websocket_error = function() {
+        self.add_message("WebSocket connection failed");
+        setTimeout(self.open_socket, 1000);
+    }
+
+    self.on_offer_created = function(sd) {
+        if (!self.peer_connection) return;
+        self.add_message("Offer created" + sd);
+        self.roulette_ws.send(JSON.stringify(sd));
+        self.offer = sd;
+    }
+
+    self.on_offer_failed = function(error) {
+        self.add_message("createOffer failed" + error);
+    }
+
+    self.on_answer_created = function(sd) {
+        if (!self.peer_connection) return;
+        self.add_message("Answer created");
+        self.peer_connection.setLocalDescription(sd);
+        self.roulette_ws.send(JSON.stringify(sd));
+    }
+
+    self.on_answer_failed = function(error) {
+        self.add_message("createAnswer failed" + error);
+    }
+
+    self.on_remotestream_added = function(evt) {
+        self.add_message("Remote stream added");
+        attachMediaStream($("#remoteVideo")[0], evt.stream);
+    }
+
+    self.on_ice_candidate = function(evt) {
+        if (evt.candidate) {
+            self.roulette_ws.send(JSON.stringify({
+                type: 'candidate',
+                label: evt.candidate.sdpMLineIndex,
+                id: evt.candidate.sdpMid,
+                candidate: evt.candidate.candidate
+            }));
+        }
+    }
+
+    $(document).ready(self.on_document_ready);
+}($);
